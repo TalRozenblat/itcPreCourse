cc7bb5834d729c76f3aa1f6a8cfce0458b8452b0
Tal <rozenblattal@gmail.com>
2022-01-03 18:12:12 +0200

    changed maps to work with 1 iFrame, added contact form, general css

--- itcprecourse.js
+++ itcprecourse.js
@@ -13,13 +13,22 @@ fetch(GITHUB_URL)
 const prevBtn = document.querySelector('.prev');
 const nextBtn = document.querySelector('.next');
 let currentlySelected = 0;
-const nodes = document.querySelectorAll('.map-frame');
+const map = document.querySelector('.map-frame');
+
+
+const cities = ["https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d54199.7719645519!2d34.806349!3d31.893396999999997!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502b70592bfe58f%3A0xeeadf8cb1758f957!2sRehovot!5e0!3m2!1sen!2sil!4v1641221879301!5m2!1sen!2sil",
+
+"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d108499.68821452232!2d34.658154!3d31.808234000000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502a3117547e4ef%3A0x97806d1a5a86632f!2sAshdod!5e0!3m2!1sen!2sil!4v1641221927387!5m2!1sen!2sil",
+
+"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d108169.8896978942!2d34.797246!3d32.087932!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4ca6193b7c1f%3A0xc1fb72a2c0963f90!2sTel%20Aviv-Yafo!5e0!3m2!1sen!2sil!4v1641221969019!5m2!1sen!2sil",
+
+"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d224040.02992289845!2d-106.064618!3d28.670931000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea449d5d484033%3A0xb7f1a7a706dd1d7b!2sChihuahua%2C%20Mexico!5e0!3m2!1sen!2sil!4v1641221995911!5m2!1sen!2sil"]
+
 
 function previous() {
   nextBtn.disabled = false;
-  nodes[currentlySelected].classList.remove("active");
   currentlySelected--;
-  nodes[currentlySelected].classList.add("active");
+  map.src = cities[currentlySelected];
 
   if (currentlySelected === 0) {
     prevBtn.disabled = true;
@@ -28,11 +37,9 @@ function previous() {
 
 function next() {
   prevBtn.disabled = false;
-  nodes[currentlySelected].classList.remove("active");
   currentlySelected++;
-  nodes[currentlySelected].classList.add("active");
-
-  if (currentlySelected + 1 === nodes.length) {
+  map.src = cities[currentlySelected];
+  if (currentlySelected + 1 === cities.length) {
     nextBtn.disabled = true;
   }
 }
