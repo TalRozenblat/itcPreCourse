e9b1533bc7288212c398f748735e8e2b8174f991
Tal <rozenblattal@gmail.com>
2022-01-06 03:12:41 +0200

    fixed form

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,19 @@
 const GITHUB_URL = "https://api.github.com/users/TalRozenblat";
 
 fetch(GITHUB_URL)
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
     const profileImage = document.getElementById("profile-image");
-    profileImage.src = data.avatar_url;
+    if (profileImage) {
+      profileImage.src = data.avatar_url;
+    }
     const profileName = document.getElementById("myName");
-    profileName.innerHTML = data.name;
+    if (profileName) {
+      profileName.innerHTML = data.name;
+    }
+
   });
 
 // map solution
@@ -20,11 +25,11 @@ const map = document.querySelector('.map-frame');
 
 const cities = ["https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d54199.7719645519!2d34.806349!3d31.893396999999997!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502b70592bfe58f%3A0xeeadf8cb1758f957!2sRehovot!5e0!3m2!1sen!2sil!4v1641221879301!5m2!1sen!2sil",
 
-"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d108499.68821452232!2d34.658154!3d31.808234000000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502a3117547e4ef%3A0x97806d1a5a86632f!2sAshdod!5e0!3m2!1sen!2sil!4v1641221927387!5m2!1sen!2sil",
+  "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d108499.68821452232!2d34.658154!3d31.808234000000002!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1502a3117547e4ef%3A0x97806d1a5a86632f!2sAshdod!5e0!3m2!1sen!2sil!4v1641221927387!5m2!1sen!2sil",
 
-"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d108169.8896978942!2d34.797246!3d32.087932!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4ca6193b7c1f%3A0xc1fb72a2c0963f90!2sTel%20Aviv-Yafo!5e0!3m2!1sen!2sil!4v1641221969019!5m2!1sen!2sil",
+  "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d108169.8896978942!2d34.797246!3d32.087932!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4ca6193b7c1f%3A0xc1fb72a2c0963f90!2sTel%20Aviv-Yafo!5e0!3m2!1sen!2sil!4v1641221969019!5m2!1sen!2sil",
 
-"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d224040.02992289845!2d-106.064618!3d28.670931000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea449d5d484033%3A0xb7f1a7a706dd1d7b!2sChihuahua%2C%20Mexico!5e0!3m2!1sen!2sil!4v1641221995911!5m2!1sen!2sil"]
+  "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d224040.02992289845!2d-106.064618!3d28.670931000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea449d5d484033%3A0xb7f1a7a706dd1d7b!2sChihuahua%2C%20Mexico!5e0!3m2!1sen!2sil!4v1641221995911!5m2!1sen!2sil"]
 
 
 function previous() {
@@ -46,11 +51,11 @@ function next() {
   }
 }
 
-prevBtn?.addEventListener("click", function() {
+prevBtn?.addEventListener("click", function () {
   previous();
 });
 
-nextBtn?.addEventListener("click", function(e) {
+nextBtn?.addEventListener("click", function () {
   next();
 });
 
@@ -58,37 +63,43 @@ nextBtn?.addEventListener("click", function(e) {
 
 //printing form to log solution
 
-const submitBtn = document.getElementById('form');
-submitBtn.addEventListener("submit",  send);
+const submitAct = document.getElementById('form');
+submitAct.addEventListener("submit", send);
+const submitBtn = document.getElementById('submitBtn');
+
+const fName = document.getElementById('firstName');
+const lName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const comment = document.getElementById('comment');
 
 
-
-function findIceCreamChoice(){
-  let flavour = document.getElementsByName('ic');
-
-  for (let i = 0; i < flavour.length; i++){
-    if(flavour[i].checked){
-      return flavour[i].value;
-    }
-  }
-}
-let fName = document.getElementById('firstName');
-  let lName = document.getElementById('lastName');
-  let email = document.getElementById('email');
-  let comment = document.getElementById('comment');
-
-function send (event) {
+function send(event) {
   event.preventDefault()
   event.stopPropagation()
-  
-  console.log("First name:", fName.value);
-  console.log("Last name:", lName.value);
-  console.log("Emaill", email.value);
-  console.log("Comment:",comment.value);
-  console.log("Favourite ice cream flavour:", findIceCreamChoice());
+  const inputs = document.querySelectorAll('.to-print')
 
+  inputs.forEach((input) => {
+    if (input.name === 'ic' && input.checked) {
+      console.log(`Favourite ice cream flavour : ${input.value}`);
+    } else if (!(input.name === 'ic')){
+      console.log(`${input.name}: ${input.value}`);
+    }
+  })
 }
 
+const formNodes = document.querySelectorAll('.formField');
+for (let i = 0; i < formNodes.length; i++) {
+  formNodes[i].onchange = checkFields
+}
+
+function checkFields() {
+  if (fName.value && email.value && comment.value && comment.value) {
+    submitBtn.removeAttribute("disabled");
+  }
+  else {
+    submitBtn.setAttribute("disabled", true);
+  }
+}
 
 //sticky nav bar solution
 window.onscroll = function() {navBarFix()};
